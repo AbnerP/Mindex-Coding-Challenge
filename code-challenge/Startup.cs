c0cff29ff9751f540cb49713bc9056f4b64cd669
Abner Pena <abnrr.pena@gmail.com>
2022-02-20 15:14:31 -0500

    Compensation Service Created and Added to Startup

--- code-challenge/Startup.cs
+++ code-challenge/Startup.cs
@@ -33,9 +33,11 @@ namespace code_challenge
                 options.UseInMemoryDatabase("EmployeeDB");
             });
             services.AddScoped<IEmployeeRepository,EmployeeRespository>();
+            services.AddScoped<ICompensationRepository,CompensationRepository>();
             services.AddTransient<EmployeeDataSeeder>();
             services.AddScoped<IEmployeeService, EmployeeService>();
             services.AddScoped<IReportingStructureService, ReportingStructureService>();
+            services.AddScoped<ICompensationService, CompensationService>();
             services.AddScoped<IMapper, Mapper>();
             services.AddMvc();
         }
