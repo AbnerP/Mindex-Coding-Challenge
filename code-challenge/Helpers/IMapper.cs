f274e18455b0fb01d707821ddf823da8189e2a25
Abner Pena <abnrr.pena@gmail.com>
2022-02-20 15:05:58 -0500

    Mapping from CompensationDto to Compensation

--- code-challenge/Helpers/IMapper.cs
+++ code-challenge/Helpers/IMapper.cs
@@ -6,5 +6,6 @@ namespace challenge.Helpers
     public interface IMapper
     {
         Employee EmployeeDto_To_Employee(EmployeeDto employeeDto);
+        Compensation CompensationDto_To_Compensation(CompensationDto compensationDto);
     }
 }
